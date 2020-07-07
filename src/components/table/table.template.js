2793922d24487794d42631125179d309a6d59f68
Eduard Shvetsov <eduard.shvetsov@anvileight.com>
2020-07-07 12:27:42 +0300

    Finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,18 +3,27 @@ const CODES = {
     Z: 90
 };
 
-function toCell() {
-    return `<div class="cell" contenteditable></div>`;
+function toCell(col) {
+    return `<div class="cell" data-col="${col}" data-type="resizable" contenteditable></div>`;
 }
 
 function toColumn(col) {
-    return `<div class="column">${col}</div>`;
+    return `<div class="column" data-type="resizable" data-col="${col}">
+                ${col}
+                <div class="col-resize" data-resize="col"></div>
+            </div>
+    `;
 }
 
 function createRow(content, index) {
+    const resize = index ? `<div class="row-resize" data-resize="row"></div>` : '';
+
     return `
-        <div class="row">
-            <div class="row-info">${index ? index : ''}</div>
+        <div class="row" data-type="resizable">
+            <div class="row-info">
+                ${index ? index : ''}
+                ${resize}
+            </div>
             <div class="row-data">${content}</div>
         </div>
     `;
@@ -24,20 +33,19 @@ function toChar(_, index) {
     return String.fromCharCode(CODES.A + index);
 }
 
+function getTemplateWithChar(count, create) {
+    return new Array(count)
+        .fill('')
+        .map(toChar)
+        .map(create)
+        .join('');
+}
+
 export function createTable(rowsCount = 15) {
     const colsCount = CODES.Z - CODES.A + 1;
     const rows = [];
-
-    const cols = new Array(colsCount)
-        .fill('')
-        .map(toChar)
-        .map(toColumn)
-        .join('');
-
-    const cells = new Array(colsCount)
-        .fill('')
-        .map(toCell)
-        .join('');
+    const cols = getTemplateWithChar(colsCount, toColumn);
+    const cells = getTemplateWithChar(colsCount, toCell);
 
     rows.push(createRow(cols));
 
