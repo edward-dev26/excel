e351f4960afdf3dbb02836e5e3b38b519f15391e
Eduard Shvetsov <eduard.shvetsov@anvileight.com>
2020-07-21 17:06:03 +0300

    Fix router test

--- src/core/router /Router.test.js
+++ src/core/router /Router.test.js
@@ -2,11 +2,13 @@ import {Router} from './Router';
 import {Page} from '../page/Page';
 
 class DashboardPage extends Page {
-    getRoot() {
-        const root = document.createElement('div');
-        root.innerHTML = 'dashboard';
+    async getRoot() {
+        return new Promise(resolve => {
+            const root = document.createElement('div');
+            root.innerHTML = 'dashboard';
 
-        return root;
+            setTimeout(() => resolve(root), 500);
+        });
     }
 }
 
@@ -30,7 +32,13 @@ describe('Router', () => {
     });
 
     test('Should render Dashboard Page', () => {
-        router.changePageHandler();
-        expect($root.innerHTML).toBe(`<div>dashboard</div>`);
+        return new Promise(resolve => {
+            router.changePageHandler();
+
+            setTimeout(() => {
+                expect($root.innerHTML).toBe(`<div>dashboard</div>`);
+                resolve();
+            }, 1000);
+        });
     });
 });
